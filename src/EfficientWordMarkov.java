7021c9e63ecae2d978a2de6078063105c1219ec4
whysample <zaw4@duke.edu>
2018-10-19 01:05:13 -0400

    pleaseee12

---
+++ src/EfficientWordMarkov.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EfficientWordMarkov extends BaseWordMarkov{
+	private Map<WordGram, ArrayList<String>> myMap;
+
+	public EfficientWordMarkov(int order) {
+		super(order);
+		myMap = new HashMap<>();
+	}
+
+	public EfficientWordMarkov() {
+		this(3);
+	}
+	public void setTraining(String text) {
+		myWords = text.split("\\s+");
+		this.myMap = new HashMap<>();
+		for (int i = 0; i < myWords.length-this.myOrder+1; i++) {
+			WordGram ch= new WordGram(myWords,i,this.myOrder);
+			ArrayList<String> name = new ArrayList<String>();
+			if (!(this.myMap.containsKey(ch))) {
+				this.myMap.put(ch, name);
+			}
+			if(i+this.myOrder+1<=text.length()) {
+				this.myMap.get(ch).add(myWords[i+1]);
+			}
+			else {
+				this.myMap.get(ch).add(PSEUDO_EOS);
+			}
+		}
+	}
+
+
+}
