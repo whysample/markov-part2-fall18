de6769a6410fc6e3b1b1a3a2549d36f81c0341a6
whysample <zaw4@duke.edu>
2018-10-18 21:31:24 -0400

    check

---
+++ src/EfficientMarkov.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class EfficientMarkov extends BaseMarkov{
+	Map<String,ArrayList<String>> myMap; 
+	
+	public EfficientMarkov(int order) {
+		super(order);
+		myMap= new HashMap<>();
+	}
+	
+	public EfficientMarkov() {
+		this(3);
+	}
+	
+	@Override
+	public void setTraining(String text) {
+		this.myMap=new HashMap<>();
+		for(int i=0;i<(text.length()-this.myOrder);i++) {
+			String c= myText.substring(i, i+this.myOrder);
+			ArrayList<String> name= new ArrayList<String>();
+			if(!(this.myMap.containsKey(c))){
+				this.myMap.put(c,name);
+			}
+			if(i+this.myOrder!=text.length()) {
+				this.myMap.get(c).add((text.substring(i+this.myOrder, 1+i+this.myOrder)));
+			}
+		}
+	}
+	
+	@Override
+	public ArrayList<String> getFollows(String key){
+		if(!(this.myMap.containsKey(key))) {
+			throw new NoSuchElementException(key+" not in map");
+		}
+		return this.myMap.get(key);
+	}
+}
+
--- src/MarkovDriver.java
+++ src/MarkovDriver.java
@@ -37,7 +37,7 @@ public class MarkovDriver {
 		
 		File f = new File(filename);
 		String text = TextSource.textFromFile(f);
-		MarkovInterface<String> standard = new BaseMarkov();
+		MarkovInterface<String> standard = new EfficientMarkov();
 		//MarkovInterface<String> efficient = new EfficientMarkov();
 		MarkovInterface<WordGram> wmm = new BaseWordMarkov();
 		//MarkovInterface<WordGram> ewm = new EfficientWordMarkov();
