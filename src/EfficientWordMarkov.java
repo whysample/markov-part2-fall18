d2598f791fb3edc477ed343c9f6744fd6c833db8
whysample <zaw4@duke.edu>
2018-10-19 01:17:10 -0400

    phew

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
-public class EfficientWordMarkov extends BaseWordMarkov{
+public class EfficientWordMarkov extends BaseWordMarkov {
 	private Map<WordGram, ArrayList<String>> myMap;
 
 	public EfficientWordMarkov(int order) {
@@ -13,24 +14,30 @@ public class EfficientWordMarkov extends BaseWordMarkov{
 	public EfficientWordMarkov() {
 		this(3);
 	}
+
 	@Override
 	public void setTraining(String text) {
 		myWords = text.split("\\s+");
 		this.myMap = new HashMap<>();
-		for (int i = 0; i < myWords.length-this.myOrder+1; i++) {
-			WordGram ch= new WordGram(myWords,i,this.myOrder);
+		for (int i = 0; i < myWords.length - this.myOrder + 1; i++) {
+			WordGram ch = new WordGram(myWords, i, this.myOrder);
 			ArrayList<String> name = new ArrayList<String>();
 			if (!(this.myMap.containsKey(ch))) {
 				this.myMap.put(ch, name);
 			}
-			if(i+this.myOrder+1<=myWords.length) {
-				this.myMap.get(ch).add(myWords[i+1]);
-			}
-			else {
+			if (i + this.myOrder + 1 <= myWords.length) {
+				this.myMap.get(ch).add(myWords[i + 1]);
+			} else {
 				this.myMap.get(ch).add(PSEUDO_EOS);
 			}
 		}
 	}
 
+	public ArrayList<String> getFollows(WordGram key) {
+		if (!(this.myMap.containsKey(key))) {
+			throw new NoSuchElementException(key + " not in map");
+		}
+		return this.myMap.get(key);
 
+	}
 }
