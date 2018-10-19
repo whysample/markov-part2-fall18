02596b102ace0b79192814cb76413b3dbe935f23
whysample <zaw4@duke.edu>
2018-10-18 22:26:35 -0400

    please

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -1,36 +1,36 @@
 import java.util.*;
-public class EfficientMarkov extends BaseMarkov{
-	Map<String,ArrayList<String>> myMap; 
-	
+
+public class EfficientMarkov extends BaseMarkov {
+	Map<String, ArrayList<String>> myMap;
+
 	public EfficientMarkov(int order) {
 		super(order);
-		myMap= new HashMap<>();
+		myMap = new HashMap<>();
 	}
-	
+
 	public EfficientMarkov() {
 		this(3);
 	}
-	
+
 	@Override
 	public void setTraining(String text) {
-		this.myMap=new HashMap<>();
-		for(int i=0;i<(text.length()-this.myOrder);i++) {
-			String c= text.substring(i, i+this.myOrder);
-			ArrayList<String> name= new ArrayList<String>();
-			if(!(this.myMap.containsKey(c))){
-				this.myMap.put(c,name);
+		this.myMap = new HashMap<>();
+		for (int i = 0; i < (text.length() - this.myOrder); i++) {
+			String c = text.substring(i, i + this.myOrder);
+			ArrayList<String> name = new ArrayList<String>();
+			if (!(this.myMap.containsKey(c))) {
+				this.myMap.put(c, name);
 			}
-			this.myMap.get(c).add((text.substring(i+this.myOrder, 1+i+this.myOrder)));
+			this.myMap.get(c).add((text.substring(i + this.myOrder,1+i + this.myOrder)));
 
 		}
 	}
-	
+
 	@Override
-	public ArrayList<String> getFollows(String key){
-		if(!(this.myMap.containsKey(key))) {
-			throw new NoSuchElementException(key+" not in map");
+	public ArrayList<String> getFollows(String key) {
+		if (!(this.myMap.containsKey(key))) {
+			throw new NoSuchElementException(key + " not in map");
 		}
 		return this.myMap.get(key);
 	}
 }
-
