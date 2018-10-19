e9b3461f00bb7169b829737f254ea304f14c3cd0
whysample <zaw4@duke.edu>
2018-10-19 18:21:35 -0400

    donefinally

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -87,9 +87,9 @@ public class Benchmark {
 		File file = new File("data/"+fileName);
 		double[] data;
 		String source = TextSource.textFromFile(file);
-		int[] sizes = {100,200,400,800,1600,3200,6400,12800};
-		int order = 5;
-		MarkovInterface<String> model = new BaseMarkov(order);
+		int[] sizes = {2000};
+		int order = 64;
+		MarkovInterface<String> model = new EfficientMarkov(order);
 		
 		// call benchmark and ignore value, first trial generates bogus data
 		data = benchmark(model,source,1000);
